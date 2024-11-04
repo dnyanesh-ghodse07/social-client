e931f7a7f676928ed68902e72eb9f0fad71d322b
dev_danny <dnyaneshghodse07@gmail.com>
2024-11-04 20:10:40 +0530

    socket url fix

--- src/socket.ts
+++ src/socket.ts
@@ -3,7 +3,7 @@ import { io } from 'socket.io-client';
 
 // http://localhost:5173/
 // "https://stoked-keyword-436905-f9.el.r.appspot.com/"
-const socket = io("http://localhost:9000/", {
+const socket = io("https://stoked-keyword-436905-f9.el.r.appspot.com/", {
   withCredentials: true,
   transports: ["polling", "websocket"]
 });
